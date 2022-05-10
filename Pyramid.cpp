efe2217fcce376265fb3c4b1e455cf997b494b71
Suman Kalyan Giri <sumankalyangiri786@gmail.com>
2022-05-10 12:57:42 +0530

    Pyramid

--- Pyramid.cpp
+++ Pyramid.cpp
@@ -8,3 +8,25 @@ Pyramid
 * * * * *
 
 */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter size of pyramid (n): ";
+    cin >> n;
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 1; j <= n - i; j++)
+        {
+            cout << " ";
+        }
+        for(int k = 1; k <= i; k++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
