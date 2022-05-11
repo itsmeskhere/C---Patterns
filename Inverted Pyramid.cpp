e57c117c4b06c1f87c0ffad73657aee2bd687738
Suman Kalyan Giri <sumankalyangiri786@gmail.com>
2022-05-11 13:20:56 +0530

    Inverted Pyramid

--- Inverted Pyramid.cpp
+++ Inverted Pyramid.cpp
@@ -17,4 +17,16 @@ int main()
     int n;
     cout << "Enter size of pyramid (n): ";
     cin >> n;
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 1; j < i; j++)
+        {
+            cout << " ";
+        }
+        for(int k = i; k <= n; k++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
 }
