932cd2ad6773681db6803bd5db91d55fefc577f7
Suman Kalyan Giri <sumankalyangiri786@gmail.com>
2022-05-14 18:16:36 +0530

    Inverted Half Pyramid

--- Inverted Half Pyramid.cpp
+++ Inverted Half Pyramid.cpp
@@ -8,3 +8,22 @@ Inverted Half Pyramid
 * 
 
 */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter size of pyramid (n): ";
+    cin >> n;
+    for(int i = n; i > 0; i--)
+    {
+        for(int j = 1; j <= n; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
