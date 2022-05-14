693faef7459f52a8c9f65a3a25545ed3a9ec217c
Suman Kalyan Giri <sumankalyangiri786@gmail.com>
2022-05-14 17:59:27 +0530

    Half Pyramid

--- Half Pyramid.cpp
+++ Half Pyramid.cpp
@@ -7,3 +7,22 @@ Half Pyramid
 * * * * *
 
 */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter size of pyramid (n): ";
+    cin >> n;
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 0; j < i; j++) 
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
