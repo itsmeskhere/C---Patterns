cb21b8fcc46b9ff50f20d92f8565c085d7ddf072
Suman Kalyan Giri <sumankalyangiri786@gmail.com>
2022-05-14 18:07:12 +0530

    Half Pyramid

--- Half Pyramid.cpp
+++ Half Pyramid.cpp
@@ -19,7 +19,7 @@ int main()
     cin >> n;
     for(int i = 1; i <= n; i++)
     {
-        for(int j = 0; j < i; j++) 
+        for(int j = 1; j <= i; j++) 
         {
             cout << "* ";
         }
