75939db5395874b2ef00e5ab6389e5962114bd5f
Suman Kalyan Giri <sumankalyangiri786@gmail.com>
2022-05-16 12:14:47 +0530

    Mirrored Inverted Half Pyramid

--- Mirrored Inverted Half Pyramid.cpp
+++ Mirrored Inverted Half Pyramid.cpp
@@ -8,3 +8,25 @@ Mirrored Inverted Half Pyramid
         *
 
 */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter size of pyramid (n): ";
+    cin >> n;
+    for(int i = 1; i <= n; i++) 
+    {
+        for(int j = 1; j < i; j++) 
+        {
+            cout << "  ";
+        }
+        for(int k = i; k <= n; k++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
